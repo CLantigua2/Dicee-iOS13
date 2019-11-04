269b1093ae5b93cdb65ffe22b4e58d6089f58b4a
Carlos Lantigua <clantigua954@gmail.com>
2019-11-03 23:35:39 -0500

    Completed code for dice game

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let images :[UIImage] = [#imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        leftDiceImageView.image  = images[Int.random(in: 0...5)]
+        rightDiceImageView.image = images[Int.random(in: 0...5)]
+    }
+    
 }
 
